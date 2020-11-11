0702aeda0e305d238400fcc6625a1166892f8c5f
Lukas Reisinger <hello@lukasreisinger.com>
2020-11-12 00:01:40 +0100

    Added 2nd line of code to test commit from IntellJ

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -1,5 +1,6 @@
 public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!! Itz me Luki");
+        System.out.println("2nd line of code");
     }
 }
